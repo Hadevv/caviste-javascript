f5b3869ad286ba9809468a3c49ba0637835bf4ff
=Fugushiva <2111jedelodder@student.epfc.eu>
2023-10-19 12:24:14 +0200

    add node and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#AE1829',
+        secondary:'#DCBF89',
+      },
+      fontFamily: {
+
+      }
+    },
+  },
+  plugins: [],
+}
