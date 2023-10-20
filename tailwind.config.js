9a36329cc8f711f68509389f4aec34f3297b8266
=Fugushiva <2111jedelodder@student.epfc.eu>
2023-10-20 12:42:26 +0200

    first lines of style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,26 +3,30 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     screens: {
-      'sm': '640px',
+      sm: "640px",
       // => @media (min-width: 640px) { ... }
-      'md': '768px',
+      md: "768px",
       // => @media (min-width: 768px) { ... }
-      'lg': '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
-      'xl': '1280px',
+      xl: "1280px",
       // => @media (min-width: 1280px) { ... }
-      '2xl': '1536px',
+      "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
     },
-    colors :{
-      primary : "#AE1829",
+    colors: {
+      primary: "#AE1829",
       // => rouge
       secondary: "#DCBF89",
       // => beige
       background: "#FFFFF2",
       // => blanc cassé
-      commentBg: "DCBF89"
+      commentBg: "DCBF89",
       // => beige foncé
+      inputBg: "#F6EFD8",
+      // => beige clair
+      borderInput: "#475069",
+      // => gris foncé
     },
     fontFamily: {
       niconne: ["Niconne", "sans-serif"],
