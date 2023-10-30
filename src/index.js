114cfeb456d9a96c026398295c512fa7459eeed4
=Fugushiva <2111jedelodder@student.epfc.eu>
2023-10-30 21:06:49 +0100

    ajout de la fonctionnalité tri par couleur

--- src/index.js
+++ src/index.js
@@ -11,6 +11,12 @@ const data = await response;
 console.log(data); //Tableau des vins
 const swiperContainer = document.querySelector(".swiper"); //L'ensemble du swiper
 const inputName = document.getElementById("inputName"); //élément input pour le nom du vin
+let tabName = []; //tableau des noms de vins
+
+//boucle de récupération du nom du vin
+for (let i = 0; i < data.length; i++) {
+  tabName.push(data[i].name);
+}
 
 // crée une slide pour chaque vin
 data.forEach((wineObject) => {
@@ -18,27 +24,55 @@ data.forEach((wineObject) => {
   //ajoute la slide à la div swiper-wrapper
   swiperContainer.querySelector(".swiper-wrapper").appendChild(slide);
 });
-//Initialisation du swiper
-newSwiper(swiperContainer);
 
-// Écoutez l'événement de recherche
+newSwiper(swiperContainer); //Initialisation du swiper
+
+/****************************************************************** */
+/*     EVENT : CHERCHER PAR NOM                                     */
+/******************************************************************* */
 inputName.addEventListener("input", (e) => {
-  // Supprimez les slides existantes
-  swiperContainer.querySelector(".swiper-wrapper").innerHTML = "";
-  // Filtrer les données
-  const winesTabFiltre = filterResults(data, inputName.value);
+  swiperContainer.querySelector(".swiper-wrapper").innerHTML = ""; // Supprimez les slides existantes
+
+  const winesTabFiltre = filterResults(data, inputName.value); // Filtrer les données
   // Créer les slides filtrées
   winesTabFiltre.forEach((wineObject) => {
     const filtreSlide = creerSlide(wineObject);
     swiperContainer.querySelector(".swiper-wrapper").appendChild(filtreSlide);
   });
+  //si la valeur de l'input est vide
   if (inputName.value.length === 0) {
-    //si la valeur de l'input est vide
     swiperContainer.querySelector(".swiper-wrapper").innerHTML = ""; // Supprimer les slides existantes
     // afficher toutes les slides
     data.forEach((wineObject) => {
       const slide = creerSlide(wineObject);
       swiperContainer.querySelector(".swiper-wrapper").appendChild(slide);
     });
+  }
+});
+
+/************************************************************************************************** */
+/*EVENT: CHERCHER PAR COULEUR */
+/**************************************************************************************************** */
+
+const colorSelector = document.getElementById("colorSelector");
+let colorWanted = ""; //la couleur qu'on souhaite
+
+colorSelector.addEventListener("change", (e) => {
+  const winesTabFiltre = filterResults(data, inputName.value); // Filtrer les données
+  colorWanted = colorSelector.value;
+  swiperContainer.querySelector(".swiper-wrapper").innerHTML = ""; //vider le slider
+ 
+  //Pour chaque vin
+  if(colorWanted == "red" || colorWanted == "pink" || colorWanted == "white"){
+    for (let wineObject of data) {
+      if (wineObject.color == colorWanted) {
+        const filtreSlide = creerSlide(wineObject);
+        swiperContainer.querySelector(".swiper-wrapper").appendChild(filtreSlide);
+      }
+    }
+    //Si la couleur a une autre valeur affiche une alerte
+  }else{
+    alert("la couleur n'existe pas")
+
   }
 });
